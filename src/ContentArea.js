cf5653aaf70cb7a1aa9390de51e674a3f04be756
Arkady Elterman <elterman@gmail.com>
2022-01-24 11:38:26 -0500

    .

--- src/ContentArea.js
+++ src/ContentArea.js
@@ -1,14 +1,14 @@
 import './App.css';
-import { useRecoilState } from 'recoil';
+import { useRecoilState, useRecoilValue } from 'recoil';
 import { useEffect } from 'react';
-import { my_assets, do_refresh } from './atoms';
-import { useInterval } from './hooks';
+import { do_refresh, my_symbols, assets_data } from './atoms';
 import _ from 'lodash';
 import { formatPercent } from './utils';
 
 const ContentArea = () => {
   const [refresh, setRefresh] = useRecoilState(do_refresh)
-  const [assetsData, setAssetsData] = useRecoilState(my_assets);
+  const [assetsData, setAssetsData] = useRecoilState(assets_data);
+  const symbols = useRecoilValue(my_symbols)
 
   useEffect(() => {
     if (!refresh) {
@@ -17,13 +17,9 @@ const ContentArea = () => {
 
     setRefresh(false);
 
-    const adata = { ...assetsData };
-    let i = 0;
-
-    const updateAssetsData = () => ++i === _.size(assetsData) && setAssetsData(adata);
-
-    _.each(assetsData, (ob, key) => {
-      const url = `https://data.messari.io/api/v1/assets/${key}/metrics/market-data`;
+    const doFetch = (i = 0, adata = {}) => {
+      const symbol = symbols[i];
+      const url = `https://data.messari.io/api/v1/assets/${symbol}/metrics/market-data`;
 
       fetch(url)
         .then((res) => {
@@ -31,42 +27,45 @@ const ContentArea = () => {
         })
         .then((data) => {
           const d = data.data;
-          adata[key] = { ...d, up: d.market_data.price_usd >= assetsData[key]?.market_data.price_usd };
+          adata[symbol] = { data: d, up: d.market_data.price_usd >= assetsData[symbol]?.market_data.price_usd };
 
-          updateAssetsData();
+          let ms = 100;
+
+          if (i === symbols.length - 1) {
+            setAssetsData(adata);
+
+            i = -1;
+            adata = {};
+            ms = 5000;
+          }
+
+          setTimeout(() => doFetch(++i, adata), ms);
         })
         .catch((err) => {
-          console.log(err);
-          updateAssetsData();
+          alert(err.status);
         });
-    })
-  }, [assetsData, refresh, setAssetsData, setRefresh])
+    }
 
-  const doRefresh = () => setRefresh(true);
+    doFetch();
+  }, [assetsData, refresh, setAssetsData, setRefresh, symbols])
 
-  useInterval(doRefresh, 10000);
+  return <div style={{ background: '#123', fontFamily: 'Roboto Mono', fontSize: '18px', height: '100vh', display: 'grid', placeContent: 'start center', padding: '10px 0' }}>
+    {_.map(assetsData, (ob) => {
+      const data = ob.data.market_data;
+      const last1 = data.percent_change_usd_last_1_hour;
+      const last24 = data.percent_change_usd_last_24_hours;
 
-  return assetsData &&
-    <div style={{ background: '#234', fontFamily: 'Roboto Mono', fontSize: '16px', height: '100vh', display: 'grid', placeContent: 'start center', padding: '10px 0' }} onClick={doRefresh}>
-      {_.map(assetsData, (ob) => {
-        if (!ob) {
-          return null;
-        }
-
-        const data = ob.market_data;
-        const last1 = data.percent_change_usd_last_1_hour;
-        const last24 = data.percent_change_usd_last_24_hours;
-
-        return (
-          <div key={ob.symbol} style={{ padding: '5px', display: 'grid', grid: 'auto / 60px 100px 80px 80px', gridAutoFlow: 'column', color: '#B8AF8F' }}>
-            <span>{ob.symbol}</span>
-            <span style={{ color: ob.up > 0 ? '#56BF8B' : '#F4425A', justifySelf: 'end' }}>{data.price_usd.toFixed(4)}</span>
-            <span style={{ color: last1 > 0 ? '#56BF8B' : '#F4425A', justifySelf: 'end' }}>{formatPercent(last1 / 100)}</span>
-            <span style={{ color: last24 > 0 ? '#56BF8B' : '#F4425A', justifySelf: 'end' }}>{formatPercent(last24 / 100)}</span>
-          </div>
-        )
-      })}
-    </div>;
+      return (
+        <div key={ob.data.symbol} style={{ padding: '5px', display: 'grid', grid: 'auto / 55px 110px 80px 80px', gridAutoFlow: 'column', color: '#B8AF8F' }}>
+          <span>{ob.data.symbol}</span>
+          <span style={{ color: ob.up > 0 ? '#56BF8B' : '#F4425A', justifySelf: 'end' }}>{data.price_usd.toFixed(4)}</span>
+          <span style={{ color: last1 > 0 ? '#56BF8B' : '#F4425A', justifySelf: 'end' }}>{formatPercent(last1 / 100)}</span>
+          <span style={{ color: last24 > 0 ? '#56BF8B' : '#F4425A', justifySelf: 'end' }}>{formatPercent(last24 / 100)}</span>
+        </div>
+      )
+    })}
+    <a href="https://messari.io/" style={{ textDecoration: 'none', color: '#fff4', fontSize: '14px' }}>Powered by Messari</a>
+  </div>;
 
 }
 
--- src/atoms.js
+++ src/atoms.js
@@ -1,7 +1,5 @@
 import { atom } from "recoil";
 
 export const do_refresh = atom({ key: 'do_refresh', default: true });
-export const my_assets = atom({ key: 'assets_data',
-    default: { BTC: null, ETH: null, ALGO: null, ADA: null, NANO: null, CHAINLINK: null, LTC: null, XLM: null, AAVE: null, },
-    dangerouslyAllowMutability: true
-});
+export const my_symbols = atom({ key: 'my_symbols', default: ['BTC', 'ETH', 'ALGO', 'ADA', 'NANO', 'CHAINLINK', 'LTC', 'XLM', 'AAVE'] });
+export const assets_data = atom({ key: 'assets_data', default: [], dangerouslyAllowMutability: true })
